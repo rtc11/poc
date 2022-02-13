58ae9b0f8354ecd629f2452a51a2117848e7f818
Robin <robin.tordly@nav.no>
2022-02-13 14:39:31 +0100

    wip

--- app/kafka-streams-json/test/ks/GenerateDiagrams.kt
+++ app/kafka-streams-json/test/ks/GenerateDiagrams.kt
@@ -4,20 +4,10 @@ import kstream.topology.KStreamsUML
 import org.junit.jupiter.api.Test
 
 class GenerateDiagrams {
-
     @Test
     fun `create UML diagram of Topology`() {
         KStreamsUML.file(createTopology()).also {
             println("Generated UML to ${it.absoluteFile}. Use in https://plantuml-editor.kkeisuke.dev")
         }
     }
-
-//    @Test
-//    fun `create topology description`() {
-//        val filename = "topology.txt"
-//        File(filename).apply {
-//            writeText(createTopology().describe().toString())
-//        }
-//        println("Topology description file generated: $filename")
-//    }
 }
